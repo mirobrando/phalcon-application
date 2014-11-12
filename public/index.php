2ecde1a869a8532039f725a36586d5aab856e66f
miroslaw.brambor <miroslaw.brambor@allegro.pl>
2014-11-12 12:42:11 +0100

    change in index

--- public/index.php
+++ public/index.php
@@ -3,6 +3,19 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+if (!file_exists(__DIR__ . '/' . $_SERVER['REQUEST_URI'])) {
+    @list($url, $other) = explode('?', $_SERVER['REQUEST_URI']);
+    $_GET['_url'] = $url;
+    if ($other) {
+        $params = explode('&', $other);
+        foreach ($params as $param) {
+            list($key, $value) = explode('=', $param);
+            $_GET[$key] = $value;
+        }
+    }
+}
+
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = new \mirolabs\phalcon\Framework\Application(__DIR__ . '/../');
