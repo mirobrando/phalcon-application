c5d204be1490efab4789fe9f3acd8f4cea5830b9
miroslaw.brambor <miroslaw.brambor@allegro.pl>
2014-12-06 21:14:38 +0100

    fix

--- public/index.php
+++ public/index.php
@@ -15,9 +15,9 @@ if (!file_exists(__DIR__ . '/' . $_SERVER['REQUEST_URI'])) {
     }
 }
 
-$env = getenv('env')?getenv('env'):\mirolabs\phalcon\Framework\Application::ENVIRONMENT_DEV;
-
 require __DIR__ . '/../vendor/autoload.php';
+
+$env = getenv('env')?getenv('env'):\mirolabs\phalcon\Framework\Application::ENVIRONMENT_DEV;
 
 $app = new \mirolabs\phalcon\Framework\Application(__DIR__ . '/../', $env);
 $app->main();
